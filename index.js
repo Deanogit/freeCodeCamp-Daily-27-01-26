86e8c1d2d378b8a3fc87cbe46e7edc69f97a7346
Dean Howe <hideanhowe@gmail.com>
2026-02-09 10:51:59 +0000

    solution

--- index.js
+++ index.js
@@ -6,11 +6,19 @@
 // For example, given 1769472000000, a timestamp for January 27th, 2026, return "odd" because the day number (27) is an odd number.
 
 function oddOrEvenDay(timestamp) {
-  console.log(timestamp);
-  console.log(new Date(timestamp).getDate());
-  const date = new Date(timestamp).getDate();
+  // console.log(timestamp)
+  // console.log(new Date(timestamp).getDate())
+  console.log(new Date(timestamp));
+  const date = new Date(timestamp).getUTCDate();
 
-  return date % 2 === 0 ? 'odd' : 'even';
+  console.log(date);
+  return date % 2 === 0 ? 'even' : 'odd';
 
   // return timestamp;
 }
+
+oddOrEvenDay(1769472000000); // odd
+oddOrEvenDay(1769444440000); // even
+oddOrEvenDay(6739456780000); // odd
+oddOrEvenDay(1); // odd
+oddOrEvenDay(86400000); // even
